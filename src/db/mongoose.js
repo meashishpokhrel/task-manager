ac031f7aff83d09023622f124fb6c90fff87c732
meashishpokhrel <meashishpokhrel@gmail.com>
2020-06-26 14:51:25 +0545

    resource creation parameters endoints for Tasks

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -8,14 +8,3 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api",{
 
 
 
-const task = new mongoose.model("task",{
-    description:{
-        type: String,
-        trim: true,
-        required: true
-    },
-    completed:{
-        type: Boolean,
-        default: false
-    }
-})
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require ("express")
 require("./db/mongoose")
 const User = require("./models/users")
+const task = require("./models/task")
 const app = express()
 
 const port = process.env.port || 3000
@@ -9,9 +10,19 @@ app.use (express.json())
 app.post("/users", (req,res) => {
     const user = new User(req.body)
     
-    user.save().then((user) => {
-        res.send(user)
+    user.save().then(() => {
+        res.status(201).send(user)
     }).catch((err) => {
+        res.status(400).send(err)
+    })
+})
+
+app.post("/tasks", (req,res) =>{
+    const Task = new task (req.body)
+
+    Task.save().then (() =>{
+        res.status(201).send(Task)
+    }).catch((err) =>{
         res.status(400).send(err)
     })
 })
