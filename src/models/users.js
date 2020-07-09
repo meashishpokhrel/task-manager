61373223115be7db6a0d817dde71a88ffe8032c9
meashishpokhrel <meashishpokhrel@gmail.com>
2020-07-09 13:41:20 +0545

    Private Data Hiding using toJSON

--- src/models/users.js
+++ src/models/users.js
@@ -2,6 +2,7 @@ const mongoose = require ("mongoose")
 const validator = require("validator")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
+// const { delete } = require("../routers/tasks")
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -49,6 +50,16 @@ const userSchema = new mongoose.Schema({
         }
     }]
 })
+
+userSchema.methods.toJSON = function() {
+    const user = this
+    const userObject = user.toObject()
+    delete userObject.password
+    delete userObject.tokens
+
+    return userObject
+    
+}
 
 userSchema.methods.generateToken = async function(){
     const user = this
