6d0ca124620dc2bdb11d9f1be119f546b8fa57d7
meashishpokhrel <meashishpokhrel@gmail.com>
2020-09-01 20:09:44 +0545

    Ui added for signup in task manager

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require ("express")
+const path = require("path")
+const hbs=require("hbs")
 require("./db/mongoose")
 const app = express()
 
@@ -8,10 +10,33 @@ const taskRouter = require ("./routers/users")
 const adminRouter = require ("./routers/admin")
 
 const port = process.env.port || 3000
+app.use(express.urlencoded({ extended: false }));
 app.use (express.json())
 app.use(userRouter)
 app.use(taskRouter)
 // app.use(adminRouter)
+
+
+//Define Paths for Express COnfig
+const PublicDirecPath = path.join(__dirname, "../public")
+const viewPath = path.join (__dirname, "../templates/views")
+const partialsPath = path.join (__dirname, "../templates/partials")
+
+//Setup handlebars engine and views location 
+app.set('view engine', 'hbs')
+app.set("views", viewPath)
+hbs.registerPartials(partialsPath);
+
+
+//Setup static directory to serve
+app.use(express.static(PublicDirecPath))
+
+app.get("/",(req,res) => {
+    res.render("index",{
+        title: "Weather",
+        name: "Ashish Pokhrel"
+    })
+})
 
 
 
@@ -22,6 +47,7 @@ app.listen(port,  () => {
 
 const Task = require ("./models/task")
 const User = require ("./models/users")
+const router = require("./routers/tasks")
 const main = async () => {
     // const task = await Task.findById("5f1bcd7bc45b421450e16bc8")
     // await task.populate("owner").execPopulate()
--- src/models/users.js
+++ src/models/users.js
@@ -42,7 +42,8 @@ const userSchema = new mongoose.Schema({
                 throw new Error("Age cannot be Negative Number!")
             }
         }
-    },
+    }
+    ,
     tokens: [{
         token:{
             type: String,
